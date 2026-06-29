4dbda60178d0250fa84fc277fa0f2a4a38cd9ae0
pratham sharma <sharma.pratham2006@gmail.com>
2026-06-29 20:18:41 +0530

    added gunicorn

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from db import jobs_collection
 from pymongo.errors import PyMongoError
+from scheduler import start_scheduler_thread
 
 app = Flask(__name__)
 CORS(app)
+
+start_scheduler_thread()
 
 @app.route("/", methods=["GET"])
 def home():
@@ -18,10 +22,17 @@ def get_jobs():
         jobs = list(jobs_collection.find({}, {"_id": 0}))
         return jsonify(jobs)
     except PyMongoError as e:
-        return jsonify({"error": "Database error", "details": str(e)}), 500
-
-from scheduler import start_scheduler_thread
+        return jsonify({
+            "error": "Database error",
+            "details": str(e)
+        }), 500
 
 if __name__ == "__main__":
-    start_scheduler_thread()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,
+        use_reloader=False
+    )
